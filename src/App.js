60c14f07d850f3852a2a95fbe4df1136c704b19a
Smith Ringtho <sringtho@gmail.com>
2023-05-08 21:49:47 +0300

    Added markdown file for initial loadup of the markdown" "

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function App() {
+  const [markdown, setMarkdown] = useState("")
+  useEffect(()=> {
+    fetch("markdown.md")
+    .then(res => res.text())
+    .then(data => setMarkdown(data))
+  },[])
+  
+
   return (
     <div className="app">
-      <h2>Smith Ringtho</h2>
+
+      <section className="editor-container">
+        <div className="editor-header">
+          <i className="fa-brands fa-free-code-camp"></i>
+          <h3>Editor</h3>
+          <i className="fa fa-arrows-alt"></i>
+        </div>
+        <textarea id="editor">
+          Smith Ringtho
+        </textarea>
+      </section>
+      <section className="preview-container">
+        <div className="preview-header">
+          <i className="fa-brands fa-free-code-camp"></i>
+          <h3>Previewer</h3>
+          <i className="fa fa-arrows-alt"></i>
+        </div>
+        <div id="preview">
+          <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+        </div>
+      </section>
     </div>
   );
 }
