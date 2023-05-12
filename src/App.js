c5322323bd72eedb532373f00c0016916301fd41
Smith Ringtho <sringtho@gmail.com>
2023-05-12 08:35:57 +0000

    Fixed the states for expanding the editor and previewer

--- src/App.js
+++ src/App.js
@@ -4,29 +4,57 @@ import remarkGfm from 'remark-gfm'
 
 function App () {
   const [markdown, setMarkdown] = useState('')
+  const [isEditorOpen, setIsEditorOpen] = useState(false)
+  const [isPreviewerOpen, setIsPreviewerOpen] = useState(false)
   useEffect(() => {
     fetch('markdown.md')
       .then(res => res.text())
       .then(data => setMarkdown(data))
   }, [])
 
+  const handleClick = (form) => {
+    if (form === 'editor') {
+      setIsEditorOpen(!isEditorOpen)
+    } else if (form === 'previewer') {
+      setIsPreviewerOpen(!isPreviewerOpen)
+    }
+  }
+
   return (
     <div className="app">
-
-      <section className="editor-container">
+      <section
+        className={`editor-container ${isPreviewerOpen ? 'previewer-open' : ''}`}>
         <div className="editor-header">
           <i className="fa-brands fa-free-code-camp"></i>
           <h3>Editor</h3>
-          <i className="fa fa-arrows-alt"></i>
+          {!isEditorOpen &&
+            <i className="fa fa-arrows-alt" onClick={
+              () => handleClick('editor')}></i>
+          }
+          {isEditorOpen &&
+            <i className="fa fa-compress-arrows-alt" onClick={
+              () => handleClick('editor')}></i>
+          }
         </div>
-        <textarea id="editor" onChange={e => setMarkdown(e.target.value)} value={markdown}>
+        <textarea id="editor"
+        className={`${isEditorOpen ? 'textarea-full' : 'textarea-closed'}`}
+          onChange={e => setMarkdown(e.target.value)}
+          value={markdown}>
         </textarea>
       </section>
-      <section className="preview-container">
+      <section
+        className={`preview-container ${isEditorOpen ? 'editor-open' : ''}`}>
         <div className="preview-header">
           <i className="fa-brands fa-free-code-camp"></i>
           <h3>Previewer</h3>
-          <i className="fa fa-arrows-alt"></i>
+          {!isPreviewerOpen &&
+          <i className="fa fa-arrows-alt" onClick={
+            () => handleClick('previewer')}></i>
+          }
+          {isPreviewerOpen &&
+            <i className="fa fa-compress-arrows-alt" onClick={
+              () => handleClick('previewer')}></i>
+          }
         </div>
         <div id="preview">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
